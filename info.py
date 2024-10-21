b1c50e408fad4f9f70d61c96a8d86b3448da2547
Virendra-7x <Virendra7x@gmail.com>
2024-10-21 09:32:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/EL_PROFESSOR_REQU')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/ELprofessorbackup')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_download_hdj')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'EL_PROFESSOR_REQU') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ELprofessorbackup) # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
