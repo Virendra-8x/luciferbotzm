71b2c9ea3d4626849bf4f2c14c4e38ed1bf3a0cb
Virendra-7x <Virendra7x@gmail.com>
2024-10-22 08:25:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -228,7 +228,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b><strong><i href='https://t.me/ELprofessorbackup'>{file_name}</i><strong>
+    CAPTION = """<b><a href='https://t.me/ELprofessorbackup'>{file_name}</a>
 </b>""" 
 
     IMDB_TEMPLATE_TXT = """
