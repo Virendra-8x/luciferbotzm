7d47ec7200c66b44a9de1284510e822fbc25f92b
Virendra-7x <Virendra7x@gmail.com>
2025-01-05 16:44:28 +0530

    Update stream.py

--- plugins/Extra/stream/stream.py
+++ plugins/Extra/stream/stream.py
@@ -1,50 +1,57 @@
 from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import STREAM_MODE, URL, LOG_CHANNEL
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 from TechVJ.util.human_readable import humanbytes
 import humanize
-import random
 
 @Client.on_message(filters.private & filters.command("stream"))
 async def stream_start(client, message):
-    if STREAM_MODE == False:
-        return 
-    msg = await client.ask(message.chat.id, "**Now send me your file/video to get stream and download link**")
-    if not msg.media:
-        return await message.reply("**Please send me supported media.**")
-    if msg.media in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
-        file = getattr(msg, msg.media.value)
-        filename = file.file_name
-        filesize = humanize.naturalsize(file.file_size) 
-        fileid = file.file_id
-        user_id = message.from_user.id
-        username =  message.from_user.mention 
+    if not STREAM_MODE:
+        return await message.reply("**Streaming is disabled.**")
 
-        log_msg = await client.send_cached_media(
-            chat_id=LOG_CHANNEL,
-            file_id=fileid,
-        )
-        fileName = {quote_plus(get_name(log_msg))}
-        stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
- 
-        await log_msg.reply_text(
-            text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
-            quote=True,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
-                                                InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
-        )
-        rm=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-                    InlineKeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ 📥', url=download)
-                ]
-            ] 
-        )
-        msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
+    # Ask user for file
+    msg = await client.ask(
+        message.chat.id, 
+        "**Now send me your file/video to generate stream and download links.**"
+    )
 
-        await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(msg)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
+    # Check if valid media
+    if not msg.media or msg.media not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.DOCUMENT]:
+        return await message.reply("**Please send a valid video or document.**")
+
+    # Extract file details
+    file = getattr(msg, msg.media.value)
+    filename = file.file_name
+    filesize = humanize.naturalsize(file.file_size)
+    fileid = file.file_id
+    user_id = message.from_user.id
+    username = message.from_user.mention
+
+    # Send media to log channel and get message ID
+    log_msg = await client.send_cached_media(chat_id=LOG_CHANNEL, file_id=fileid)
+    encoded_file_name = quote_plus(get_name(log_msg))
+    file_hash = get_hash(log_msg)
+
+    # Generate URLs
+    stream_url = f"{URL}watch/{log_msg.id}/{encoded_file_name}?hash={file_hash}"
+    download_url = f"{URL}{log_msg.id}/{encoded_file_name}?hash={file_hash}"
+
+    # Buttons
+    buttons = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("🖥 Stream", url=stream_url)],
+            [InlineKeyboardButton("📥 Download", url=download_url)],
+        ]
+    )
+
+    # Send response to user
+    await message.reply_text(
+        text=f"**𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱!**\n\n"
+             f"📂 **File Name:** `{filename}`\n"
+             f"📦 **File Size:** `{filesize}`\n\n"
+             f"👇 **Choose an option below:**",
+        reply_markup=buttons,
+        disable_web_page_preview=True
+    )
