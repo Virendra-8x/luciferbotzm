24f3eaf029da02623d5d48c8efe59908d63d62ad
Virendra-8x <virendra6x@gmail.com>
2025-01-07 18:05:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,8 +52,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Abhi:tXNuqfZ8rEXzz0zW@cluster0.rf4vpki.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "luciferbotz")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'lucifercollection')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Abhi")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'telegram_files')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', False)) # Set Ture Or False
